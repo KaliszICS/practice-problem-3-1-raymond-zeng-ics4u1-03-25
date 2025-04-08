0da525c971bb23706804d636422cecbca702a05e
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-04-08 00:09:39 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,23 @@ public class PracticeProblem {
 
 	}
 
+	public static int sumOfDigits(int x) {
+        if (x < 0) {
+            x = x * -1;
+        }
+        if (x < 10) {
+            return x;
+        }
+        return (x % 10) + sumOfDigits(x / 10);
+    }
 	
-
+	public static int factorial(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+}
 }
